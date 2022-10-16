5a64387db5b78a914ac557512b44a3b6391bb7c3
ramadan8 <79653940+ramadan8@users.noreply.github.com>
2022-10-16 20:25:03 +0200

    Update logging format.

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,7 @@
 import logging
 import logging.handlers
 import os
+import zlib
 
 from .bot import bot
 
@@ -14,12 +15,23 @@ stream_formatter = logging.Formatter(
 )
 
 file_formatter = logging.Formatter(
-    "%(created)i/%(name)s/%(levelname)s/%(message)s"
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(stream_formatter)
+
+def namer(name):
+    return name + ".gz"
+
+def rotator(source, dest):
+    with open(source, "rb") as sf:
+        data = sf.read()
+        compressed = zlib.compress(data, 9)
+        with open(dest, "wb") as df:
+            df.write(compressed)
+    os.remove(source)
 
 file_handler = logging.handlers.TimedRotatingFileHandler(
     filename="./logs/discord.log",
@@ -28,6 +40,9 @@ file_handler = logging.handlers.TimedRotatingFileHandler(
     backupCount=7,
     encoding="utf-8",
 )
+
+file_handler.rotator = rotator
+file_handler.namer = namer
 
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(file_formatter)
--- test/test_utility.py
+++ test/test_utility.py
@@ -3,15 +3,15 @@ import unittest
 from src import utility
 
 class UtilityTest(unittest.TestCase):
+    def test_timestamp_valid_s(self):
+        self.assertEqual(utility.timestamp_to_seconds("10"), 10)
+
     def test_timestamp_valid_ms(self):
         self.assertEqual(utility.timestamp_to_seconds("3:45"), 225)
     
     def test_timestamp_valid_hms(self):
         self.assertEqual(utility.timestamp_to_seconds("01:34:21"), 5661)
     
-    def test_timestamp_valid_s(self):
-        self.assertEqual(utility.timestamp_to_seconds("10"), 10)
-
     def test_timestamp_invalid(self):
         def _test_timestamp_invalid():
             try:
